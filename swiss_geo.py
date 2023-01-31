36e726e535e92cc209e299d1cd1f2396853b27c3
todortodor <lepot.simon@gmail.com>
2023-01-31 10:32:11 +0100

    data cleaning swiss_geo

--- swiss_geo.py
+++ swiss_geo.py
@@ -31,15 +31,15 @@ d = {'ids': ['1','2'],
      'unemployment': [0.0, 12.0]
     }
 
-fig = px.choropleth_mapbox(d, geojson=shapes, 
-                           locations='ids', 
-                           color='unemployment',
-                           mapbox_style="carto-positron",
-                           zoom=6.3, 
-                           center = {"lat": 46.8, "lon": 8.5},
-                           opacity=0.5,
-                          )
-fig.show()
+# fig = px.choropleth_mapbox(d, geojson=shapes, 
+#                            locations='ids', 
+#                            color='unemployment',
+#                            mapbox_style="carto-positron",
+#                            zoom=6.3, 
+#                            center = {"lat": 46.8, "lon": 8.5},
+#                            opacity=0.5,
+#                           )
+# fig.show()
 
 gdf=gpd.GeoDataFrame(columns=shapes.columns)
 
@@ -56,14 +56,36 @@ gdf=gdf.drop(columns=['KT_TEIL']) # For the sake of clean data ;-)
 gdf=gdf.rename(columns={'EINWOHNERZ':'Inhabitants'})
 
 gdf = gdf[['geometry','NAME','Inhabitants','KANTONSNUM']]
-fig = px.choropleth_mapbox(gdf, geojson=gdf.geometry,
-                           locations=gdf.index,
-                           color='Inhabitants', 
-                           hover_name = 'NAME',
-                           color_continuous_scale='algae',
-                           mapbox_style="carto-positron",
-                           zoom=7.3, 
-                           center = {"lat": 46.8, "lon": 8.5},
-                           opacity=0.5,
-                           )
-fig.show()
+
+#%%
+import pandas as pd
+cantons_of_switzerland = pd.read_csv('Cantons_of_switzerland_1.csv')
+cantons_of_switzerland = cantons_of_switzerland[['Unnamed: 0','Code']]
+cantons_of_switzerland.columns = ['KANTONSNUM','canton_code']
+cantons_of_switzerland = cantons_of_switzerland.iloc[:-1]
+cantons_of_switzerland['KANTONSNUM'] = cantons_of_switzerland['KANTONSNUM'].astype('int')
+
+new_gdf = pd.merge(cantons_of_switzerland,gdf, on = 'KANTONSNUM')
+new_gdf.columns = ['canton_number', 'canton_code', 'geometry', 'canton_name', 'inhabitants']
+new_gdf['canton_number'] = new_gdf['canton_number'].astype('int')
+new_gdf['canton_code'] = new_gdf['canton_code'].astype('str')
+
+
+new_gdf_gpd = gpd.GeoDataFrame(new_gdf)
+new_gdf_gpd.to_csv('data_for_map.csv',index=False)
+new_gdf_gpd.to_file("data_for_map.gpkg", driver="GPKG")
+# for style in ['open-street-map', 'white-bg', 'carto-positron', 
+#               'carto-darkmatter']:
+for style in ['stamen-terrain']:
+    fig = px.choropleth_mapbox(new_gdf_gpd, geojson=new_gdf_gpd.geometry,
+                                locations=new_gdf_gpd.index,
+                                color='inhabitants', 
+                                hover_name = 'canton_name',
+                                color_continuous_scale='algae',
+                                mapbox_style=style,
+                                zoom=7.3, 
+                                title = style,
+                                center = {"lat": 46.8, "lon": 8.5},
+                                opacity=0.5,
+                                )
+    fig.show()
